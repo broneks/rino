4765c3e51fb32a5ee637dd2594eea874f4cd13cd
broneks <bronek@fastmail.fm>
2016-01-24 21:01:08 -0500

    added gulp-sass, gulp-livereload, gulp-autoprefixer and a server task

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,16 @@
 const gulp = require('gulp');
 const webpack = require('webpack-stream');
 const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 const plumber = require('gulp-plumber');
+const livereload = require('gulp-livereload');
+const http = require('http');
+const st = require('st');
 
 const tasks = [
-  'scripts'
+  'scripts',
+  'styles'
 ];
 
 gulp.task('scripts', () => {
@@ -30,10 +36,37 @@ gulp.task('scripts', () => {
     .pipe(webpack(config))
     .pipe(uglify())
     .pipe(gulp.dest('dist'))
+    .pipe(livereload())
+});
+
+gulp.task('styles', () => {
+  gulp.src('sass/main.scss')
+    .pipe(plumber())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(livereload())
+});
+
+gulp.task('server', (done) => {
+  http.createServer(
+    st({
+      path: __dirname,
+      index: 'index.html',
+      cache: false
+    })
+  ).listen(8080, done);
+});
+
+gulp.task('watch', tasks.concat(['server']), () => {
+  livereload.listen();
+  gulp.watch('src/**/*.js', ['scripts']);
+  gulp.watch('sass/**/*.scss', ['styles']);
 });
 
 gulp.task('default', tasks);
-
-gulp.task('watch', tasks, () => {
-  gulp.watch('src/**/*.js', ['scripts']);
-});
